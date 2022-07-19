87c6380f794684e409938a63793458eb6cf38259
Yi-Ching <yictseng@student.42.fr>
2022-07-19 19:12:22 +0200

    add css: body and app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import './App.css'
-import { ReactComponent as IconStar } from './images/icon-star.svg'
+// import { ReactComponent as IconStar } from './images/icon-star.svg'
 
 function App() {
-  return (
-    <div className='App'>
-      <p>hello</p>
-      <IconStar />
-    </div>
-  )
+  return <div className='App'>{/* <IconStar /> */}</div>
 }
 
 export default App
